0ad421a4014c5b18637d0d31d6b7a0834090c2fb
Adam Moss <adammoss7@gmail.com>
2014-10-03 16:22:40 +0100

    Enabled blocks in config.xml - admin/config banner added

---
+++ app/code/community/Creare/CreareSeoCore/Block/Adminhtml/System/Config/Fieldset/Info.php
@@ -0,0 +1,31 @@
+<?php
+
+class Creare_CreareSeoCore_Block_Adminhtml_System_Config_Fieldset_Info 
+	extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface {
+
+		protected $_template = 'creareseo/system/config/fieldset/info.phtml';
+
+		public function render(Varien_Data_Form_Element_Abstract $element) {
+        	return $this->toHtml();
+    	}
+
+    	protected function getModuleVersion() {
+        	return (string) Mage::getConfig()->getNode('modules/Creare_CreareSeoCore/version');
+    	}
+
+    	protected function getCreareWebsite() {
+        	return (string) "https://www.creare.co.uk";
+    	}
+
+    	protected function getCreareHelpDesk() {
+        	return (string) "https://www.creare.co.uk/services/helpdeskultimate/customer/";
+    	}
+
+    	protected function getCreareBlogPost() {
+        	return (string) "https://www.creare.co.uk/creare-seo-magento-extension";
+    	}
+
+    	protected function getGitHubPage() {
+        	return (string) "https://github.com/Creare/CreareSEO";
+    	}
+	}
