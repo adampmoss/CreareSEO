e6d4ffd847d7b254e0cf70fca691772b46d05f4e
Simon Sprankel <sprankhub@users.noreply.github.com>
2017-06-09 17:27:05 +0200

    added compatibility with Wyomind_Cmstree

--- app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head/Cmscanonical.php
+++ app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head/Cmscanonical.php
@@ -19,6 +19,10 @@ class Creare_CreareSeoCore_Block_Page_Html_Head_Cmscanonical extends Mage_Core_B
             $canonicalUrl
         );
 
+        if (Mage::helper('core')->isModuleEnabled('Wyomind_Cmstree')) {
+            $canonicalUrl .= Mage::helper('cmstree')->getUrlSuffix();
+        }
+
         $addSlash = $this->creareseoHelper()->getConfig('cms_canonical_slashes');
 
         if ($addSlash) {
